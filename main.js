9d7d897ff61704e46a13b986de9e91242ac392eb
IlaySkutelsky <40234996+IlaySkutelsky@users.noreply.github.com>
2020-05-10 22:43:02 +0300

    no glitch, no pink

--- main.js
+++ main.js
@@ -4,14 +4,3 @@ function showGif() {
   headerElm.classList.add("hidden")
   gifElm.classList.remove("hidden")
 }
-
-function showGlitch() {
-  if (Math.random()>0.34) return
-  let glitchElm = document.querySelector(".gif .glitch")
-  glitchElm.classList.remove("hidden")
-}
-
-function hideGlitch() {
-  let glitchElm = document.querySelector(".gif .glitch")
-  glitchElm.classList.add("hidden")
-}
